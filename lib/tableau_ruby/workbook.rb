66a3f40695a9d4d0d1575a3bc20986c9fb1d11d7
Jon Calvert <jonathan.calvert@kitcheck.com>
2015-11-10 11:34:45 -0500

    Default to overwrite workbooks

--- lib/tableau_ruby/workbook.rb
+++ lib/tableau_ruby/workbook.rb
@@ -55,6 +55,7 @@ BODY
       resp = @client.conn.post("/api/2.0/sites/#{params[:site_id]}/workbooks") do |req|
         req.options.timeout = 300 # open/read timeout sould be high...if your workbook talks to datasources, the http request can time out before its done
         req.options.open_timeout = 300
+        req.params["overwrite"] = true
         req.headers["Content-Type"] = "multipart/mixed; boundary=\"boundary-string\""
         req.headers['X-Tableau-Auth'] = @client.token if @client.token
         req.body = multipart_body
